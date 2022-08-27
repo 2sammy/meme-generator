c209522b11f95559165b12bbd5eebdb42975ad1d
2sammy <samuelmanoah61@gmail.com>
2022-08-27 17:12:37 +0300

    event handler

---
+++ EventListener.js/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App(){
+    return(
+        <div className = "container" >
+        <img src = "https://picsum.photos/640/360" />
+        <button> click me</button>
+        </div>
+    )
+}
